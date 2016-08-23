8c7f0b2bd00b61882dbb985e4b2218cf24303bf7
thethinkman <ameer.jalal@gmail.com>
2016-08-22 21:46:01 -0400

    changed folder

--- policykitmanager/PolicyKitManager.java
+++ policykitmanager/PolicyKitManager.java
@@ -8,7 +8,7 @@ Info:     This application will manage PolicyKit policies for the end user. gksu
           for them manually. Additonally, this tempts users to just use sudo, which can be a
           security concern.
 */
-package policymanager;
+package policykitmanager;
 
 import java.io.*;         //need to read and write to policy files
 import org.gnome.gdk.*;   //needed for GUI
@@ -17,18 +17,25 @@ import org.gnome.gtk.*;   //needed for GUI
 
 public class PolicyKitManager{
   public static void main(String[] args){
-    String policyPath = "/usr/share/polkit-1/actions/org.mycustom.policykit.policy";
     try{
       //First, detect if PolicyKitManager is being run with root. This is necessary.
       System.out.println("Checking if privileged user...");
       //String output = (p.getOutputStream());
       //String error = (p.getErrorStream());
       if (!isRoot()) //check if user is root
-        throw new Exception("\nPlease run this program with administrator permissions.");
+        throw new Exception("\nPlease run with sudo to use. Otherwise, try polkit-explorer.");
+      System.out.println("Great!");
+      //Check if policy file exists, if not, create it
+      File policy = new File("/usr/share/polkit-1/actions/org.policykitmanager.policykit.policy");
+      if (!policy.isFile())
+        if (!makePolicy(policy))
+          throw new Exception("\nFailed to create policy file in /usr/share/polkit-1/actions/.");
+      BufferedReader policyReader = new BufferedReader(
+        new InputStreamReader(new FileInputStream(policy)));
+
     }catch(Exception e){
       System.out.println(e.getMessage());
     }
-
   }
 
   private static boolean isRoot() throws IOException{
@@ -41,6 +48,18 @@ public class PolicyKitManager{
       if(iAmRoot)
         return true;
       else return false;
+    }catch(IOException io){
+      return false;
+    }
+  }
+
+  private static boolean makePolicy(File newPolicy) throws IOException{
+    //Create Policy file
+    try{
+      if (newPolicy.createNewFile())  //attempt to create the file
+        return true;
+      else return false;
+
     }catch(IOException io){
       return false;
     }
